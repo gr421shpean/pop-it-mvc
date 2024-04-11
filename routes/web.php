5ef829f62780ef26f7c3f6cc271a601abdee2754
gr421shpean <149349454+gr421shpean@users.noreply.github.com>
2024-04-11 11:22:48 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Src\Route;
+
+Route::add('GET', '/hello', [Controller\Site::class, 'hello']);
+Route::add(['GET', 'POST'], '/signup', [Controller\Site::class, 'signup']);
+Route::add(['GET', 'POST'], '/login', [Controller\Site::class, 'login']);
+Route::add('GET', '/logout', [Controller\Site::class, 'logout']);
